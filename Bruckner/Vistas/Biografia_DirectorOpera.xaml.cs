023b062660cc896d9c2890ba78265faadc952ad2
rosorio1704 <83722649+rosorio1704@users.noreply.github.com>
2024-09-14 01:08:31 -0600

    Add files via upload

---
+++ Bruckner/Vistas/Biografia_DirectorOpera.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Bruckner.Vistas
+{
+    /// <summary>
+    /// Lógica de interacción para Biografia_DirectorOpera.xaml
+    /// </summary>
+    public partial class Biografia_DirectorOpera : Page
+    {
+        public Biografia_DirectorOpera()
+        {
+            InitializeComponent();
+        }
+    }
+}
