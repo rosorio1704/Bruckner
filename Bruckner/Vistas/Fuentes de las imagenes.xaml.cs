ac19bcd3e5952136f1e18fa22405e969ffc020b9
osoriori <osoriori@LMXMCA008597>
2024-09-14 00:29:25 -0600

    Agregar archivos de proyecto.

---
+++ Bruckner/Vistas/Fuentes de las imagenes.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Bruckner.Vistas
+{
+    /// <summary>
+    /// Lógica de interacción para Fuentes_de_las_imagenes.xaml
+    /// </summary>
+    public partial class Fuentes_de_las_imagenes : Page
+    {
+        public Fuentes_de_las_imagenes()
+        {
+            InitializeComponent();
+        }
+    }
+}
